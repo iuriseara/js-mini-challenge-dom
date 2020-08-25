d6e4cd840d6e7fd92e36be89cb090aed9a18c8c7
iuriseara <iuriseara@gmail.com>
2020-08-25 19:17:39 -0400

    Done

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 /***** Deliverable 1 *****/
 const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+// console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
+header.style.color = "red"
 
 
 /***** Deliverable 3 *****/
 // console.log("PLAYERS array looks like this:", PLAYERS)
+const playerClass = document.querySelector('.player-container')
+// console.log(playerClass)
+
+PLAYERS.forEach(function(player){
+    playerClass.innerHTML += `
+    <div class="player" data-number="${player.number}">
+      <h3>
+        ${player.name} (<em>${player.nickname}</em>)
+      </h3>
+     <img src="${player.photo}" alt="${player.name}">
+    </div> `;
+});
+
+
 
 
 /***** Deliverable 4 *****/
+
+document.querySelector(`[data-number="7"]`).remove()
